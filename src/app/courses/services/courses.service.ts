bb888566dd15074a219ed68f0acda1e04aa58736
Ivan Silvestre <isilvestreneto@gmail.com>
2022-06-02 14:01:24 -0300

    category-pipe

--- src/app/courses/services/courses.service.ts
+++ src/app/courses/services/courses.service.ts
@@ -8,7 +8,7 @@ import { Course } from './../model/course';
   providedIn: 'root',
 })
 export class CoursesService {
-  private readonly API = 'assets/acourses.json';
+  private readonly API = 'assets/courses.json';
 
   constructor(private httpClient: HttpClient) {}
 
